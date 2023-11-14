c31df6e30520ab0369ed1a92f692eb64cff162df
“lgtnice” <1798607710@qq.com>
2023-11-14 13:50:07 +0800

    加入 PPT读取模块

---
+++ src/PPTReader/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTReader
+{
+    public class Class1
+    {
+    }
+}
